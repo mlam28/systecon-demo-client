0fa28da95b3cc9ab24c53dd0eb58cb14bc1a8135
mlam28 <shelly2.lam@gmail.com>
2019-10-17 22:11:05 -0400

    set up table container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ResultChart from './containers/ResultChart'
+import ResultTable from './containers/ResultTable'
 import * as data from './FrontEndDemoData.json'
 
 const jsonData = JSON.stringify(data)
@@ -12,16 +13,27 @@ const modelTwoData = chartData.filter(info => info.MODEL ===  "Demo - Model 02")
 const modelThreeData = chartData.filter(info => info.MODEL ===  "Demo - Model 03")
 
 const tableData = objData.default["View2 - Result Table"]
+const modelOneTable = tableData.filter(info => info.MODEL === "Demo - Model 01")
+const modelTwoTable = tableData.filter(info => info.MODEL === "Demo - Model 02")
+const modelThreeTable = tableData.filter(info => info.MODEL === "Demo - Model 03")
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <script src='./FrontEndDemoData.json'></script>
-      <ResultChart data={modelOneData} dataTwo={modelTwoData} dataThree={modelThreeData}/>
-    </div>
-  );
+class App extends React.Component{
+
+  state={
+    open: false
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <script src='./FrontEndDemoData.json'></script>
+        <ResultChart data={modelOneData} dataTwo={modelTwoData} dataThree={modelThreeData}/>
+        <ResultTable />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/ResultTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ResultTable extends React.Component{
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+    
+}
+
+
+export default ResultTable
